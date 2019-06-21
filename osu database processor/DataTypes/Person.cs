fc4b84c88b787aee8056cf2d157f8a34d4576d0f
JonasHenkens <22411316+JonasHenkens@users.noreply.github.com>
2019-06-21 03:23:54 +0200

    Reading and writing presence.db works
    Person created and implemented
    PresenceDb implemented

---
+++ osu database processor/DataTypes/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace osu_database_processor.DataTypes
+{
+    class Person
+    {
+        public int PlayerId { get; private set; }
+        public string Name { get; private set; }
+        public byte Country1 { get; private set; }
+        public byte Country2 { get; private set; }
+        public byte Unknown { get; private set; } // most 0, regulary 4 and 96,  all found: 0 4 6 22 32 36 38 64 68 70 96 100 102
+        public long Unknown2 { get; private set; } // 8 unknown bytes
+        public int Rank { get; private set; }
+        public long Unknown3 { get; private set; } // 8 unknown bytes
+
+        public Person(OsuReader o)
+        {
+            ReadFromStream(o);
+        }
+
+        public void ReadFromStream(OsuReader o)
+        {
+            PlayerId = o.ReadInt32();
+            Name = o.ReadString();
+            Country1 = o.ReadByte();
+            Country2 = o.ReadByte();
+            Unknown = o.ReadByte();
+            Unknown2 = o.ReadInt64();
+            Rank = o.ReadInt32();
+            Unknown3 = o.ReadInt64();
+        }
+
+        public void WriteToStream(OsuWriter o)
+        {
+            o.Write(PlayerId);
+            o.Write(Name);
+            o.Write(Country1);
+            o.Write(Country2);
+            o.Write(Unknown);
+            o.Write(Unknown2);
+            o.Write(Rank);
+            o.Write(Unknown3);
+        }
+    }
+}
--- osu database processor/DataTypes/PresenceDb.cs
+++ osu database processor/DataTypes/PresenceDb.cs
@@ -6,5 +6,34 @@ namespace osu_database_processor.DataTypes
 {
     class PresenceDb
     {
+        public int Version { get; private set; }
+        public int Amount { get; private set; }
+        public List<Person> People { get; private set; }
+
+        public PresenceDb(OsuReader o)
+        {
+            ReadFromStream(o);
+        }
+
+        public void ReadFromStream(OsuReader o)
+        {
+            Version = o.ReadInt32();
+            Amount = o.ReadInt32();
+            People = new List<Person>();
+            for (int i = 0; i < Amount; i++)
+            {
+                People.Add(new Person(o));
+            }
+        }
+
+        public void WriteToStream(OsuWriter o)
+        {
+            o.Write(Version);
+            o.Write(Amount);
+            foreach (var person in People)
+            {
+                person.WriteToStream(o);
+            }
+        }
     }
 }
